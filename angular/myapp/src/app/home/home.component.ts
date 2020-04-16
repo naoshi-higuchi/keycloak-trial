df6a79188e391b5fd272ad83a2b16ae9256f3a61
Naoshi HIGUCHI <naoshi.higuchi@gmail.com>
2020-04-16 20:19:35 +0900

    Dump access-token & id-token by @auth0/angular-jwt.

--- angular/myapp/src/app/home/home.component.ts
+++ angular/myapp/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { JwtHelperService } from '@auth0/angular-jwt'
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,11 @@ import { OAuthService } from 'angular-oauth2-oidc';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private oauthService: OAuthService) { }
+  private jwt;
+
+  constructor(private oauthService: OAuthService) {
+    this.jwt = new JwtHelperService();
+  }
 
   ngOnInit(): void {
   }
@@ -31,6 +36,16 @@ export class HomeComponent implements OnInit {
     return accessToken;
   }
 
+  public get accessTokenDecoded() {
+    const accessTokenDecoded = this.jwt.decodeToken(this.accessToken);
+
+    if (!accessTokenDecoded) {
+      return null;
+    }
+
+    return JSON.stringify(accessTokenDecoded);
+  }
+
   public get idToken() {
     const idToken = this.oauthService.getIdToken();
 
@@ -39,5 +54,15 @@ export class HomeComponent implements OnInit {
     }
 
     return idToken;
+  }
+
+  public get idTokenDecoded() {
+    const idTokenDecoded = this.jwt.decodeToken(this.idToken);
+
+    if (!idTokenDecoded) {
+      return null;
+    }
+
+    return JSON.stringify(idTokenDecoded);
   }
 }
